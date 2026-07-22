328bfbc4ac6101054ae85ae2281991a98f95cea2
rafikidevfs <rafikidevfs@gmail.com>
2026-07-21 23:29:33 -0300

    Add Tailwind CSS plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [tailwindcss(), react(), tsconfigPaths()],
   build: {
     outDir: "dist",
   },
