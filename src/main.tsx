9919640d172e252aa2fed473b049ef316c430dc6
rafikidevfs <rafikidevfs@gmail.com>
2026-07-21 23:23:06 -0300

    Change CSS import from index.css to styles.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from '@tanstack/react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { getRouter } from './router';
-import './index.css'; // Mude para './styles.css' se o seu CSS tiver esse nome
+import './styles.css'; // Mude para './styles.css' se o seu CSS tiver esse nome
 
 const queryClient = new QueryClient();
 const router = getRouter();
