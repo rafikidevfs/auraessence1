a895459d33579886fc45d39759feff6008a0af93
rafikidevfs <rafikidevfs@gmail.com>
2026-07-21 23:31:13 -0300

    Add missing Tailwind CSS import in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from '@tailwindcss/vite'; // <--- Faltou esse import no topo!
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tailwindcss(), react(), tsconfigPaths()],
-  build: {
-    outDir: "dist",
-  },
+  plugins: [
+    tailwindcss(),
+    react(),
+    tsconfigPaths(),
+  ],
 });
