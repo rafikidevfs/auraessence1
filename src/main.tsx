c255d105a4ec37ba4c7c355e7e1bf7713c6f057c
rafikidevfs <rafikidevfs@gmail.com>
2026-07-21 23:22:05 -0300

    Refactor main.tsx to use QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import createRoot from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { RouterProvider } from '@tanstack/react-router';
-import router from './router'; // <--- Sem chaves aqui
-import './index.css'; // Ou './styles.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { getRouter } from './router';
+import './index.css'; // Mude para './styles.css' se o seu CSS tiver esse nome
+
+const queryClient = new QueryClient();
+const router = getRouter();
 
 const rootElement = document.getElementById('root')!;
+
 if (!rootElement.innerHTML) {
-  const root = createRoot(rootElement);
-  root.render(<RouterProvider router={router} />);
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </React.StrictMode>
+  );
 }
